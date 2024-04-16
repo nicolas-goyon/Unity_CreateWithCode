4911a374bc8b48b82fa160b0887b6016ea757e7e
Ciberdead <nico_goyon@orange.fr>
2024-04-16 09:24:59 +0100

    challenge done

---
+++ Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject plane;
+
+    [SerializeField]
+    private Vector3 offset = new Vector3(0, 0, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.0f;
+
+    [SerializeField]
+    private float rotationSpeed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        var verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+
+        // tilt the plane up/down based on up/down arrow keys
+        if (verticalInput != 0)
+            transform.Rotate(verticalInput * rotationSpeed * Time.deltaTime * Vector3.right);
+    }
+}
