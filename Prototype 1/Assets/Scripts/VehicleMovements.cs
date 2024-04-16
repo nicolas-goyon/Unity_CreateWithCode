879b65a4c02064d19de97e55f40f1437adc92f17
Ciberdead <nico_goyon@orange.fr>
2024-04-16 09:05:08 +0100

    Right/Left okay

--- Prototype 1/Assets/Scripts/VehicleMovements.cs
+++ Prototype 1/Assets/Scripts/VehicleMovements.cs
@@ -7,6 +7,13 @@ public class VehicleMovements : MonoBehaviour
 
     [SerializeField] 
     private float speed = 20f;
+
+    [SerializeField]
+    private float turnSpeed = 20f;
+
+    private float horizontalAxis;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +23,14 @@ public class VehicleMovements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+       // Move the vehicle forward
+       var forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(forwardInput * speed * Time.deltaTime * Vector3.forward);
         
+        // Turn the vehicle left and right
+        horizontalAxis = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up, horizontalAxis * turnSpeed * Time.deltaTime);
     }
 }
